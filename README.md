a0e2b4d0f0f1fc478286f77ae7c2cb2621fe36d4
Nuwan Rajika Kumarasiri <nuwan.kumarasiri@wisc.edu>
2024-09-30 19:04:08 -0500

    add function, struct example

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Collections of zig examples
 
 * [Hello World](intro/hello.zig)
+* [Functions, Struct, Unions, Arrays, Slices](intro/ds.zig)
---
+++ intro/ds.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() void {
+    const sum = add(9001, 2);
+    std.debug.print("9001 + 2 = {d}\n", .{sum});
+
+    const user = User{ .power = 9001, .name = "Goku" };
+    std.debug.print("{s}'s power is {d}\n", .{ user.name, user.power });
+}
+
+pub const User = struct {
+    power: u64,
+    name: []const u8,
+};
+
+fn add(a: i64, b: i64) i64 {
+    return a + b;
+}
