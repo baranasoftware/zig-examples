52ee5851bfb3968b2e9adf6487f7c1be3173c94d
Nuwan Rajika Kumarasiri <baranasoftware@gmail.com>
2025-01-18 09:35:16 -0600

    server1 is ready - blocking i/o

--- server1.zig
+++ server1.zig
@@ -6,6 +6,7 @@
 // https://domenicoluciani.com/2024/05/07/create-dns-resolver.html
 // https://github.com/EmilHernvall/dnsguide
 // https://github.com/google/gopacket/blob/master/layers/dns.go
+// nc  localhost 8443
 
 const std = @import("std");
 const net = std.net;
@@ -37,6 +38,8 @@ pub const DnsServer = struct {
         try posix.bind(listener, &address.any, address.getOsSockLen());
         try posix.listen(listener, 128);
 
+        var buf: [10]u8 = undefined;
+
         while (true) {
             var client_address: net.Address = undefined;
             var client_address_len: posix.socklen_t = @sizeOf(net.Address);
@@ -48,6 +51,25 @@ pub const DnsServer = struct {
             defer posix.close(socket);
 
             print("{} connected\n", .{client_address});
+
+            const timeout = posix.timeval{ .tv_sec = 2, .tv_usec = 500_000 };
+
+            // read timeout
+            try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
+
+            // write timeout
+            try posix.setsockopt(socket, posix.SQL.SOCKET, posix.SO.SNDTIMEO, &std.meme.toBytes(timeout));
+
+            const read = posix.read(socket, &buf) catch |err| {
+                print("error reading: {}\n", .{err});
+                continue;
+            };
+
+            if (read == 0) {
+                continue;
+            } else {
+                print("client => {s}\n", .{buf});
+            }
 
             write(socket, "hello and goodbye") catch |err| {
                 print("error writing: {}\n", .{err});
