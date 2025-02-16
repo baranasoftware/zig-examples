2918d1ee988105b9b2cf23c8bddc42494f72100a
Nuwan Rajika Kumarasiri <baranasoftware@gmail.com>
2025-02-16 16:32:47 -0600

    add task2 example

--- task2.zig
+++ task2.zig
@@ -59,32 +59,50 @@ fn Job(comptime T: type) type {
 fn Scheduler(comptime T: type) type {
     return struct {
         queue: Queue,
-        allocator: Allocator,
+        mutex: std.Thread.Mutex,
+        cond: std.Thread.Condition,
 
         const Self = @This();
 
-        const Queue = std.DoublyLinkedList(Job(T));
+        const Queue = std.PriorityQueue(Job(T), void, compare);
+
+        fn compare(_: void, a: Job(T), b: Job(T)) std.math.Order {
+            return std.math.order(a.run_at, b.run_at);
+        }
 
         pub fn init(allocator: Allocator) Self {
             return .{
-                .queue = Queue{},
-                .allocator = allocator,
+                .cond = .{},
+                .mutex = .{},
+                .queue = Queue.init(allocator, {}),
             };
         }
 
         pub fn deinit(self: *Self) void {
-            while (self.queue.pop()) |node| {
-                self.allocator.destroy(node);
-            }
+            self.queue.deinit();
         }
 
         pub fn schedule(self: *Self, task: T, run_at: i64) !void {
-            const node = try self.allocator.create(Queue.Node);
-            node.data = Job(T){
+            const job = Job(T){
                 .task = task,
                 .run_at = run_at,
             };
-            self.queue.append(node);
+            self.mutex.lock();
+            defer self.mutex.unlock();
+
+            var reschedule = false;
+            if (self.queue.peek()) |*next| {
+                if (run_at < next.run_at) {
+                    reschedule = true;
+                }
+            } else {
+                reschedule = true;
+            }
+
+            try self.queue.add(job);
+            if (reschedule) {
+                self.cond.signal(); // signal that a job is added
+            }
         }
 
         pub fn scheduleIn(self: *Self, task: T, ms: i64) !void {
@@ -93,34 +111,41 @@ fn Scheduler(comptime T: type) type {
 
         pub fn start(self: *Self) !void {
             const thread = try std.Thread.spawn(.{}, Self.run, .{self});
-            thread.join();
+            thread.detach();
         }
 
         fn run(self: *Self) void {
             while (true) {
-                const ms = self.timeUntilNextTask() orelse 1000;
-                std.time.sleep(@intCast(ms * std.time.ns_per_ms));
+                self.mutex.lock();
+                if (self.timeUntilNextTask()) |ms| {
+                    if (ms > 0) {
+                        const ns: u64 = @intCast(std.time.ns_per_ms * ms);
+                        self.cond.timedWait(&self.mutex, ns) catch |err| {
+                            std.debug.assert(err == error.Timeout);
+                        };
+                    }
+                } else {
+                    self.cond.wait(&self.mutex);
+                }
+                while (self.queue.peek() == null) {
+                    self.cond.wait(&self.mutex);
+                }
 
-                const first = self.queue.first orelse {
-                    // queue is empty, go back and star the loop
-                    continue;
-                };
+                const next = self.queue.peek() orelse continue;
 
-                const job = first.data;
-                if (job.run_at > std.time.milliTimestamp()) {
-                    // go back and start the loop again
+                if (next.run_at > std.time.milliTimestamp()) {
                     continue;
                 }
 
-                self.queue.remove(first);
-                self.allocator.destroy(first);
+                const job = self.queue.remove();
+                self.mutex.unlock();
                 job.task.run();
             }
         }
 
         fn timeUntilNextTask(self: *Self) ?i64 {
-            if (self.queue.first) |first| {
-                return std.time.milliTimestamp() - first.data.run_at;
+            if (self.queue.peek()) |*next| {
+                return next.run_at - std.time.milliTimestamp();
             }
             return null;
         }
